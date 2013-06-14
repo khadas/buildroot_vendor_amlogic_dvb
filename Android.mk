f04cb6b94be879338c8f09f6b3185449bc8b3af6
ke.gong <ke.gong@amlogic.com>
2013-06-14 16:40:16 +0800

    PD# 74834: Remove ndk header files.

--- Android.mk
+++ Android.mk
@@ -1,6 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE_TAGS := optional
-LOCAL_PREBUILT_LIBS := libam_mw.so libam_adp.so
-include $(BUILD_MULTI_PREBUILT)
+#include $(CLEAR_VARS)
+#LOCAL_MODULE_TAGS := optional
+#LOCAL_PREBUILT_LIBS := libam_mw.so libam_adp.so
+#include $(BUILD_MULTI_PREBUILT)
