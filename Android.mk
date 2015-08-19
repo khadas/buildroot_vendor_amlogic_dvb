6da6a48fc41c5938103d82870b61dde88c720aaa
Ke Gong <ke.gong@amlogic.com>
2015-08-19 17:15:41 +0800

    Add libam_ver.so to Android.mk
    Change-Id: I5a1a6d4f49dbee698039358dd55e70e53189e6b8

--- Android.mk
+++ Android.mk
@@ -2,5 +2,5 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
-LOCAL_PREBUILT_LIBS := libam_mw.so libam_adp.so
+LOCAL_PREBUILT_LIBS := libam_mw.so libam_adp.so libam_ver.so
 include $(BUILD_MULTI_PREBUILT)
