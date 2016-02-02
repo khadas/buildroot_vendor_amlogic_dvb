7c93959261322567d70dd274d2aef97b84de3391
Zhiqiang Han <zhiqiang.han@amlogic.com>
2016-02-02 20:32:19 +0800

    PD#118966: fix for 64bit
    Change-Id: I2850076a641dd9414d93ed800871b3319ff28a5f

--- Android.mk
+++ Android.mk
@@ -1,6 +1,60 @@
 LOCAL_PATH := $(call my-dir)
 
+multilib := 32
+
 include $(CLEAR_VARS)
+LOCAL_MODULE := libam_adp
+LOCAL_MODULE_SUFFIX :=.so
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_TAGS := optional
-LOCAL_PREBUILT_LIBS := libam_mw.so libam_adp.so libam_ver.so
-include $(BUILD_MULTI_PREBUILT)
+LOCAL_PRELINK_MODULE := false
+ifdef TARGET_2ND_ARCH
+LOCAL_MULTILIB := $(multilib)
+LOCAL_MODULE_PATH_64 := $(TARGET_OUT)/lib64
+LOCAL_SRC_FILES_64 := lib64/libam_adp.so
+LOCAL_MODULE_PATH_32 := $(TARGET_OUT)/lib
+LOCAL_SRC_FILES_32 := libam_adp.so
+else
+LOCAL_MODULE_PATH := $(TARGET_OUT)/lib
+LOCAL_SRC_FILES := libam_adp.so
+endif
+include $(BUILD_PREBUILT)
+
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libam_mw
+LOCAL_MODULE_SUFFIX :=.so
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_TAGS := optional
+LOCAL_PRELINK_MODULE := false
+ifdef TARGET_2ND_ARCH
+LOCAL_MULTILIB := $(multilib)
+LOCAL_MODULE_PATH_64 := $(TARGET_OUT)/lib64
+LOCAL_SRC_FILES_64 := lib64/libam_mw.so
+LOCAL_MODULE_PATH_32 := $(TARGET_OUT)/lib
+LOCAL_SRC_FILES_32 := libam_mw.so
+else
+LOCAL_MODULE_PATH := $(TARGET_OUT)/lib
+LOCAL_SRC_FILES := libam_mw.so
+endif
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libam_ver
+LOCAL_MODULE_SUFFIX :=.so
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_TAGS := optional
+LOCAL_PRELINK_MODULE := false
+ifdef TARGET_2ND_ARCH
+LOCAL_MULTILIB := $(multilib)
+LOCAL_MODULE_PATH_64 := $(TARGET_OUT)/lib64
+LOCAL_SRC_FILES_64 := lib64/libam_ver.so
+LOCAL_MODULE_PATH_32 := $(TARGET_OUT)/lib
+LOCAL_SRC_FILES_32 := libam_ver.so
+else
+LOCAL_MODULE_PATH := $(TARGET_OUT)/lib
+LOCAL_SRC_FILES := libam_ver.so
+endif
+include $(BUILD_PREBUILT)
+
+
