6ae9e4ba7fe15362c49b87aaab60443e5e093d1a
Ke Gong <ke.gong@amlogic.com>
2017-04-11 14:30:28 +0800

    build adp source
    Change-Id: I9350f563a480e8e612fda0d2cbc659f45bea40e7

--- Android.mk
+++ Android.mk
@@ -3,25 +3,6 @@ LOCAL_PATH := $(call my-dir)
 multilib := both
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := libam_adp
-LOCAL_MODULE_SUFFIX :=.so
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_TAGS := optional
-LOCAL_PRELINK_MODULE := false
-ifdef TARGET_2ND_ARCH
-LOCAL_MULTILIB := $(multilib)
-LOCAL_MODULE_PATH_64 := $(TARGET_OUT)/lib64
-LOCAL_SRC_FILES_64 := lib64/libam_adp.so
-LOCAL_MODULE_PATH_32 := $(TARGET_OUT)/lib
-LOCAL_SRC_FILES_32 := libam_adp.so
-else
-LOCAL_MODULE_PATH := $(TARGET_OUT)/lib
-LOCAL_SRC_FILES := libam_adp.so
-endif
-include $(BUILD_PREBUILT)
-
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := libam_mw
 LOCAL_MODULE_SUFFIX :=.so
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
@@ -75,3 +56,5 @@ LOCAL_SRC_FILES := libam_sysfs.so
 endif
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+include $(LOCAL_PATH)/am_adp/Android.mk
